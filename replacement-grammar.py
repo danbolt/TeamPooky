2369a64ed55290d42d2808922e7d64f1ef7c67c8
Daniel Savage <dansvg@gmail.com>
2014-02-05 18:24:21 -0800

    Replacement grammer seems to do the test input fine.

--- replacement-grammar.py
+++ replacement-grammar.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python
+
 import string
+import sys
 
 class Rule:
 	inString = ""
@@ -11,11 +14,37 @@ class Rule:
 def replaceWithRule(input, rule):
 	return string.replace(input, rule.inString, rule.outString)
 
-def parseRule(input):
-	splitInput = string.split("|")
+def makeRule(input):
+	splitInput = string.split(input, "|")
 	return Rule(splitInput[0], splitInput[1])
 
+def translateWithRules(rules, input):
+	if (len(rules) == 0):
+		return input
+	else:
+		outp = replaceWithRule(input, rules[0])
+		rules.pop(0)
+		return translateWithRules(rules, outp)
+
+
+
 def main():
+	rules = []
+	readingRules = True
+
+	while (readingRules == True):
+		s = sys.stdin.readline().rstrip('\r\n')
+		if (len(s) == 0):
+			readingRules = False
+		else:
+			rules.append(makeRule(s))
+
+	# keep reading newlines until we get the message
+	s = ""
+	while (len(s) == 0):
+		s = sys.stdin.readline().rstrip('\r\n')
+
+	print translateWithRules(rules, s)
 	#
 
 
