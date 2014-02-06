4aec6d2ba4ca185adb82370699364bf41719a9bf
Daniel Savage <dansvg@gmail.com>
2014-02-05 17:48:14 -0800

    Made some rule replacement strings

--- replacement-grammar.py
+++ replacement-grammar.py
@@ -1,7 +1,23 @@
+import string
 
+class Rule:
+	inString = ""
+	outString = ""
+	def __init__(self, inString, outString):
+		self.inString = inString;
+		self.outString = outString;
+
+
+def replaceWithRule(input, rule):
+	return string.replace(input, rule.inString, rule.outString)
+
+def parseRule(input):
+	splitInput = string.split("|")
+	return Rule(splitInput[0], splitInput[1])
 
 def main():
-	print("Hello world!");
+	#
+
 
 if __name__ == "__main__":
 	main()
